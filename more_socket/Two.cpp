561c9fab8f8be318f4c8486e501b1fea5d8fe745
CoolD69 <parthasingharoy166@gmail.com>
2022-05-07 12:13:13 +0530

    system IP

--- more_socket/Two.cpp
+++ more_socket/Two.cpp
@@ -1,45 +1,92 @@
-#include<iostream>
-#include<winsock.h>
+#include <iostream>
+#include <winsock.h>
+#include<stdio.h>
 using namespace std;
 
-//macros
+// macros
 #define PORT 8000
 #define int_32_ 32
 /***********************************************************
  * compile command = "g++ <test>.cpp -o <test2> -lws2_32"
- * compile command = "g++ One.cpp -o One -lws2_32"
- * 
- *  1.gethostname & WSAStartup
- *  2.setsockopt demo
- * 
+ * compile command = "g++ Two.cpp -o Two -lws2_32"
+ *
+ *  1.gethostbyname & WSAStartup
+ *
+ *
  *********************************************************/
 
-//structure
+// structure
 struct sockaddr_in srv;
+struct hostent *remoteHost;
+struct in_addr addr;
 
-int main(int argc, char const *argv[])
+int main()
 {
     WSADATA ws;
-    if (WSAStartup(MAKEWORD(2, 2), &ws) < 0)
-	{
-		cout << endl
-			 << "WSA failed to initialised ." << endl;
-	}
-	else
-	{
-		cout << "WSA initialised successfully !" << endl;
-	}
-    
-    char strHostName[int_32_]={0,};
-    int nRet=gethostname(strHostName,int_32_);
-    if(nRet<0){
-        cout<<"Call failed !"<<endl;
+    int iResults;
 
-        // exit();
+    char **pAlias;
+
+    iResults = WSAStartup(MAKEWORD(2, 2), &ws);
+
+    if (iResults != 0)
+    {
+        return 1;
     }
-    else{
-        cout<<"The HOST name : "<<strHostName<<endl;
+
+    char strHostName[32] = {
+        0,
+    };
+    int nRet = gethostname(strHostName, int_32_);
+    if (nRet != 0)
+    {
+        cout << endl
+             << "Not able to find the HOSTNAME. " << endl;
+        return 1;
     }
+
+    remoteHost = gethostbyname(strHostName);
+    if (strHostName[0] != '\0')
+    {
+        for (pAlias = remoteHost->h_aliases; *pAlias != 0; pAlias++)
+        {
+            cout << endl
+                 << *pAlias;
+        }
+
+        printf("\tAddress type : ");
+        switch (remoteHost->h_addrtype)
+        {
+        case AF_INET:
+            cout << endl
+                 << "AF_INET";
+            break;
+
+        case AF_NETBIOS:
+            cout << endl
+                 << "AF_NETBIOS";
+            break;
+
+        default:
+            break;
+        }
+        cout << endl
+             << "Addr len : " << remoteHost->h_length;
+        int i = 0;
+        if (remoteHost->h_addrtype == AF_INET)
+        {
+            while (remoteHost->h_addr_list[i] !=0)
+            {
+                addr.s_addr = *(u_long*)remoteHost->h_addr_list[i++];
+                cout<<endl<<inet_ntoa(addr);
+
+            }
+            
+        }
+        else if(remoteHost->h_addrtype == AF_NETBIOS){
+            cout<<endl<<"NETBIOS address was returned.0";
+        }
+    }
+
     return 0;
 }
-
