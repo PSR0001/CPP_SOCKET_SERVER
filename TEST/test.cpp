768df2f9e9143775b718bdfa604d6639c05d6855
CoolD69 <parthasingharoy166@gmail.com>
2022-05-07 23:30:32 +0530

    updated

---
+++ TEST/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <iterator>
+
+using namespace std;
+
+int main(){
+  string cmd = "ipconfig";
+  string filename = "macaddress.txt";
+  
+  system((cmd + ">" + filename).c_str());
+  string line;
+  ifstream myfile ("macaddress.txt");
+  
+  if(myfile.is_open()){
+    while(getline(myfile,line)){
+      cout<<line<<endl;
+    }
+    myfile.close();
+  }
+  else
+  cout<<"Unable to open the file";
+
+  return 0;
+}
